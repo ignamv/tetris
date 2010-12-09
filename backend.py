fa65aa420068bf04d8f3fc45a5d94d3f86b8ec1d
Ignacio Martinez V <ignamv@gmail.com>
2010-12-09 15:41:36 -0300

    Arreglé rotación y coordenadas del centro de rotación de piezas
    Ahora parece funcionar bien la rotación

--- backend.py
+++ backend.py
@@ -9,13 +9,17 @@ columnas = 10
 grilla = [[0 for i in range(columnas)] for j in range(filas)]
 colores_max = 5
 
-formas = cargarPiezas("piezas.txt")
 # Duplas con las coordenadas de cada celda que pertenece a la pieza actual
 pieza = []
 colorPieza = 0
 
+x_actual = y_actual = 0
+rotx = roty = 0
+
+actual = None
+
 def mover(dx,dy):
-	global pieza
+	global pieza, x_actual, y_actual
 	# Me fijo que haya lugar para mover la pieza y que no se salga del tablero
 	piezaNueva = []
 	for (x,y) in pieza:
@@ -30,35 +34,73 @@ def mover(dx,dy):
 	for (x,y) in piezaNueva:
 		grilla[y][x] = colorPieza
 	pieza = piezaNueva
+	x_actual += dx
+	y_actual += dy
+	return False
+
+def rotar():
+	global pieza,grilla
+	# Calculo el centro de la pieza
+	cx = x_actual + 0.5 * rotx
+	cy = y_actual + 0.5 * roty
+	print cx, cy
+	# La transformación es (x,y) = (cx-y+cy,cy+x-cx)
+	# Me fijo que haya lugar para rotar la pieza y que no se salga del tablero
+	piezaNueva = []
+	for (x,y) in pieza:
+		(nx,ny) = (int(cx-y+cy),int(cy+x-cx))
+		if ny >= filas or nx >= columnas or ny < 0 or nx < 0 or \
+			(nx,ny) not in pieza and grilla[ny][nx] != 0:
+			print "no rota (",x,y,")->(",nx,ny,")"
+			return True
+		piezaNueva.append((nx,ny))
+	# La muevo
+	for (x,y) in pieza:
+		if (x,y) not in piezaNueva:
+			grilla[y][x] = 0
+	for (x,y) in piezaNueva:
+		grilla[y][x] = colorPieza
+	pieza = piezaNueva
 	return False
 
 def cargarPiezas(archivo):
 	piezas= [[]]
+	centros = []
 	# Las piezas están en un archivo de texto, separadas por lineas vacías
+	# TODO: calcular un centro de rotación si el archivo no lo especifica
 	f = open(archivo,'r')
 	nueva = None
 	for linea in f:
 		if linea.isspace():
+			# Una línea en blanco comienza una pieza nueva
 			piezas.append([])
 			continue
-		piezas[-1].append([(0,1)[c != " "] for c in linea.rstrip()])
+		if linea[0] == "(":
+			# Coordenadas del centro de rotación
+			tmp = linea[1:-2].split(",")
+			centros.append((int(tmp[0]),int(tmp[1])))
+		else:
+			piezas[-1].append([(0,1)[c != " "] for c in linea.rstrip()])
 
 	# Ahora me aseguro que las filas de cada pieza tengan largo uniforme
 	for pieza in piezas:
 		ancho = max([len(fila) for fila in pieza])
 		for fila in pieza:
 			fila.extend([0]*(ancho-len(fila)))
-	return piezas
+	return (piezas,centros)
 
 def piezaNueva():
 	from random import random
-	global pieza, grilla, colorPieza
+	global pieza, grilla, colorPieza, y_actual, x_actual, rotx, roty
 	# Selecciono una forma al azar
-	nueva = formas[int(random()*len(formas))]
+	n = int(random()*len(formas))
+	nueva = formas[n]
+	(rotx,roty) = centros[n]
 	ancho = max([len(fila) for fila in nueva ])
 	pieza = []
 	# Posición inicial
-	dx = int(random()*(columnas-ancho))
+	y_actual = 0
+	x_actual = dx = int(random()*(columnas-ancho))
 	# Traduzco de la grilla que describe la pieza al array de coordenadas
 	for (y,fila) in enumerate(nueva):
 		for (x,celda) in enumerate(fila):
@@ -79,6 +121,7 @@ def mostrarGrilla():
 		print
 
 if __name__ == "__main__":
+	(formas,centros) = cargarPiezas("piezas.txt")
 	piezaNueva()
 	while 1:
 		# Hago caer la pieza, si da true no pudo caer
@@ -112,4 +155,6 @@ if __name__ == "__main__":
 			mover(-1,0)
 		elif entrada == "d":
 			mover(1,0)
+		elif entrada == "w":
+			rotar()
 
